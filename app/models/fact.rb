0310bac2dc795606838dc0e26fdc6229cf3bcea6
Daniel Otieno <danielotieno.ke@gmail.com>
2021-06-26 00:01:03 +0300

    chore:add model validation

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,2 +1,7 @@
 class Fact < ApplicationRecord
+  validates :text, presence: true
+  validates :source, presence: true
+  validates :username, presence: true
+  validates :stars, numericality: { in: 0..5 }
+
 end
