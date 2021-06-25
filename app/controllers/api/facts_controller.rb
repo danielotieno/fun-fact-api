26b5f6896604944ae5316a489b743fd25cde46db
Daniel Otieno <danielotieno.ke@gmail.com>
2021-06-25 23:33:07 +0300

    chore:add set fact private method

--- app/controllers/api/facts_controller.rb
+++ app/controllers/api/facts_controller.rb
@@ -1,6 +1,16 @@
 class Api::FactsController < ApplicationController
 
+  before_action :set_fact, only: [:show, :update, :destroy]
+
   def index
     render json: Fact.all
   end
+
+  private
+
+  def set_fact
+    @fact = Fact.find(params[:id])
+  end
+
+
 end
